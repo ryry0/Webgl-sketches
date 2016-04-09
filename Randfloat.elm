ab0169a83b83993ff02269b3b3724d6fb8c4ee6f
Ryan Reyes <ryan.david.reyes@gmail.com>
2016-04-09 00:04:19 -0400

    Added creation of random lists and random tuples.
    Easy monayyy.

--- Randfloat.elm
+++ Randfloat.elm
@@ -10,6 +10,27 @@ main : Signal Graphics.Element.Element
 main = Signal.map show <| genrandfloat 0 10
 --}
 
+genrandfloatlist : Int -> Float -> Float -> Signal Time.Time -> Signal (List Float)
+genrandfloatlist numelem low high time =
+  Signal.map (randfloatlist numelem low high) (timeseed time)
+
+randfloatlist : Int -> Float -> Float -> Random.Seed -> List Float
+randfloatlist numelem low high seed=
+  seed
+    |> ( Random.generate <| Random.list numelem (Random.float low high))
+    |> fst
+
+genrandfloatpair : Float -> Float -> Signal Time.Time -> Signal (Float, Float)
+genrandfloatpair low high time =
+  Signal.map (randfloatpair low high) (timeseed time)
+
+randfloatpair : Float -> Float -> Random.Seed -> (Float, Float)
+randfloatpair low high seed =
+  seed
+    |> (Random.generate
+    <| Random.pair (Random.float low high) (Random.float low high))
+    |> fst
+
 genrandfloat : Float -> Float -> Signal Time.Time -> Signal Float
 genrandfloat low high time =
   Signal.map (randfloat low high) (timeseed time)
